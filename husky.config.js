c41a247021c11a21308743d9075185c11db91f34
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-02-27 14:27:05 +0200

    move lint-staged to /web

--- husky.config.js
+++ husky.config.js
@@ -3,7 +3,7 @@ let huskyConfig = {};
 if (process.env.ENABLE_PANTHER_WEB_GIT_HOOKS) {
   huskyConfig = {
     hooks: {
-      'pre-commit': 'lint-staged',
+      'pre-commit': 'lint-staged -c web/lint-staged.config.js',
     },
   };
 }
