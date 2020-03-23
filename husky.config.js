3049ffc20f25d0b01a412005dbd59b10c4582758
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-03-23 13:17:48 +0200

    move codegen.yml inside web

--- husky.config.js
+++ husky.config.js
@@ -3,7 +3,7 @@ let huskyConfig = {};
 if (process.env.ENABLE_PANTHER_WEB_GIT_HOOKS) {
   huskyConfig = {
     hooks: {
-      'pre-commit': 'lint-staged -c web/lint-staged.config.js',
+      'pre-commit': 'lint-staged --config web/lint-staged.config.js',
     },
   };
 }
